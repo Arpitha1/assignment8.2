2d8fdfe363c07256cc6e5df47a363073f89c2f7c
Arpitha1 <arpitha.gr01@gmail.com>
2017-06-30 21:48:05 +0530

    Initial commit

---
+++ app/src/main/java/com/example/abhi/sharedpreference/Constants.java
@@ -0,0 +1,13 @@
+package com.example.abhi.sharedpreference;
+
+/**
+ * Created by Abhi on 27-06-2017.
+ */
+
+public class Constants {
+    public static final String NAME = "name";
+    public static final String AGE= "age";
+    public static final String PHONE= "phone";
+    public static final String CITY= "city";
+}
+
